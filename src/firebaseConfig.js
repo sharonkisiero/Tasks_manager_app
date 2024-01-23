8c0f8299c57d5373811c6eead75af530fc72a91d
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2024-01-23 21:58:21 +0530

    updated 3

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,14 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "INSERT_API_KEY",
-  authDomain: "INSERT_AUTH_DOMAIN",
-  projectId: "INSERT_PROJECT_ID",
-  databaseURL: "INSERT_DATABASE_URL",
-  storageBucket: "INSERT_STORAGE_BUCKET",
-  messagingSenderId: "INSERT_MESSAGING_SENDER_ID",
-  appId: "INSERT_APP_ID",
-  measurementId: "INSERT_MEASUREMENT_ID"
+  apiKey: "AIzaSyAZOrCL16FpKOAiSy16YlCfUIznwiLbjw0",
+  authDomain: "task-manager-acf48.firebaseapp.com",
+  projectId: "task-manager-acf48",
+  databaseURL: "https://task-manager-acf48-default-rtdb.firebaseio.com", // Add the databaseURL for Realtime Database
+  storageBucket: "task-manager-acf48.appspot.com",
+  messagingSenderId: "149595014920",
+  appId: "1:149595014920:web:93560c32f0f90af2e88ebd",
+  measurementId: "G-VBX1F70RLJ"
 };
 
 // Initialize Firebase
